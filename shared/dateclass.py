a7900fe9510658169a917cea76b0603236428eb9
unknown <j.mendezquesada18@gmail.com>
2024-04-30 15:50:23 -0600

    see if queue client is imported ahora si :v

--- shared/dateclass.py
+++ shared/dateclass.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import pytz
-from azure.storage.queu import QueueClient
+from azure.storage.queue import QueueClient
 
 def get_current_date():
     current_date = datetime.now(pytz.timezone('UTC')).date()
